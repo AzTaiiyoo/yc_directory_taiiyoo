bb739bfc82766c53393fd376c6ac8bae9eae6ae8
Kiran Bonhomme <kiran.bonhomme@gmail.com>
2024-11-26 20:08:15 +0100

    initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import SearchForm from "../../components/SearchForm";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+import { STARTUP_QUERY } from "@/sanity/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+  const params = { search: query || null };
+  const { data: posts } = await sanityFetch({ query: STARTUP_QUERY, params });
+
+  return (
+    <>
+      <section className='pink_container'>
+        <h1 className='heading'>
+          Pitch Your Startup <br />
+          Connect With Entrepreneurs
+        </h1>
+        <p className='sub-heading !max-w-3xl'>
+          Submit Ideas, Vote on Pitches, and get Noticed in Virtual
+          Competitions.
+        </p>
+        <SearchForm query={query} />
+      </section>
+      <section className='section_container'>
+        <p className='text-30-semibold'>
+          {query ? `Search Results for ${query}` : "All startups"}
+        </p>
+        <ul contextMenu='mt-7 card_grid'>
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post._id} post={post} />
+            ))
+          ) : (
+            <p className='no-results'>No startups found</p>
+          )}
+        </ul>
+      </section>
+      <SanityLive />
+    </>
+  );
+}
